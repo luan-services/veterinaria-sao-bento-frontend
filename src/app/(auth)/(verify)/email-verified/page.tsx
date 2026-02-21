7f66838abb1f0f7701038892de5715d495bd59e5
Luan Peixoto Jardim <luandotservices@gmail.com>
2026-02-21 19:57:06 +0000

    fix: updated email verified page design

--- src/app/(auth)/(verify)/email-verified/page.tsx
+++ src/app/(auth)/(verify)/email-verified/page.tsx
@@ -2,9 +2,29 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { useSession } from "@/src/lib/auth-client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 
-export default function EmailVerifiedPage() {
+import Image from "next/image";
+import sao_bento_logo from "@/public/sao-bento-logo.svg"
+import sao_bento_logo_dark from "@/public/sao-bento-logo-dark.svg"
+
+import { Loading } from "@/src/components/layout/Loading";
+import { Card } from "@/src/components/ui/Card";
+import { TextButton } from "@/src/components/ui/TextButton";
+
+/* even though this is a client component, next.js still tries to pre-render it on the server at build time to generate a fast, 
+static HTML file to send to the user immediately 
+
+in this specific case, on build time, pre-render will fail and cause build errors because useSearchParams() which strictly only runs on 
+client
+
+the solution for this is wrapping the page around a <Suspense> element from React, when building this static HTML file, next won't try 
+to run the code inside here, it'll  just put the fallback HTML in its place until it goes to the client 
+
+suspense cannot be wrapped inside the same component where the useSearchParams is because it needs to PREVENT it to run, so we make it
+wrapped around the component */
+
+const EmailVerifiedContent = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
 
@@ -14,116 +34,180 @@ export default function EmailVerifiedPage() {
     /* if there is any error, get it it here from the params url */
     const error = searchParams.get("error");
 
+    /* when switching tabs on browser better auth tends to recall useSession to get new session data, to prevent the 'loading' 
+    component to flicker, we usually do if(isPending && !session), but in this specific page, session sometimes can be null 
+    forever, and pending would still reload, so we need a state that says the first attempt on getting session already happened */
+    const [initialLoadPending, setInitialLoadPending] = useState(true);
+
     /* time left before redirect */
     const [timeLeft, setTimeLeft] = useState(5);
 
+    const targetUrl = error || !session?.user?.emailVerified ? "/login" : "/dashboard";
+
+    useEffect(() => { /* the moment isPending becomes false for the first time, flip initialLoad */
+        if (!isPending && initialLoadPending) {
+            setInitialLoadPending(false);
+        }
+    }, [isPending, initialLoadPending]);
+
     useEffect(() => {
         /* if it's loading, don't start the timer yet */
-        if (isPending) {
+        if (initialLoadPending) {
             return;
         }
 
-        // 1. Define where to go based on the case
-        let targetUrl = "";
-        
-        if (error || !(session?.user?.emailVerified)) {
-            targetUrl = "/login";
-        } 
-
-        if (session?.user?.emailVerified) {
-            targetUrl = "/dashboard"; 
-        } 
-
         /* update time left */
         const interval = setInterval(() => {
-            setTimeLeft((prev) => prev - 1);
+            setTimeLeft((prev) => prev > 0 ? prev - 1 : prev);
         }, 1000);
 
-        /* trigger countdown */
-        const timeout = setTimeout(() => {
-            router.push(targetUrl);
-        }, 5000);
+        if (timeLeft === 0) {
+            router.replace(targetUrl);
+            return;
+        }
 
         /* cleanup functions */
         return () => {
             clearInterval(interval);
-            clearTimeout(timeout);
         };
         
-    }, [error, session, isPending, router]);
+    }, [initialLoadPending, router, timeLeft, targetUrl]);
 
-    if (isPending) {
-        return <div className="p-10 text-center animate-pulse">Carregando...</div>; 
+    if (initialLoadPending) {
+        return (
+            <Loading />
+        );
     }
 
     /* first case: the backend returned an error in the params, that means the link is expired or invalid */
     if (error) {
         return (
-            <div className="flex min-h-screen items-center justify-center bg-slate-50 p-4 font-sans">
-                <div className="w-full space-y-4 max-w-md bg-white p-8 border text-slate-700 text-center">
-                    <h1 className="text-2xl font-semibold text-red-600">Link Inválido</h1>
-                    <p className="text-slate-600">Este link expirou ou é inválido.</p>
-                    
-                    <p className="text-sm text-slate-400">
+            <main className="flex min-h-screen items-center justify-center bg-default p-2 sm:p-8 md:p-12">
+                <Card className="max-w-200" size="xl">
+                    <div className="flex w-full justify-center">
+                        {/* must revert classnames after testing */}
+                        <Image
+                            src={sao_bento_logo}
+                            alt="Logo"
+                            className="dark:hidden w-full max-w-50 h-auto mb-2"
+                        />
+                        <Image
+                            src={sao_bento_logo_dark}
+                            alt="Logo"
+                            className="hidden dark:block w-full max-w-50 h-auto mb-2"
+                        />
+                    </div>
+                    <h1 className="p-4 text-center text-2xl font-semibold text-default-fg">
+                        Link Inválido
+                    </h1>
+
+                    <p className="pb-2 text-muted-fg text-center">
+                        Esse link de confirmação expirou ou é inválido. Tente novamente.
+                    </p>
+
+                    <p className="pb-8 text-sm text-default-fg text-center">
                         Redirecionando para login em {timeLeft}s...
                     </p>
-                    
-                    <button
-                        onClick={() => router.push('/login')}
-                        className="w-full border bg-slate-50 py-2 mt-2 hover:bg-slate-100 transition-colors"
+
+                    <TextButton
+                        className="w-full justify-center mb-2"
+                        onClick={() => router.replace('/login')}
+                        variant="primary"
                     >
-                        Voltar para o Login agora
-                    </button>
-                </div>
-            </div>
+                        Voltar para login agora
+                    </TextButton>
+                </Card>
+            </main>
         );
     }
 
     /* second case: the user is logged in and really is verified */
     if (session?.user.emailVerified) {
         return (
-            <div className="flex min-h-screen items-center justify-center bg-slate-50 p-4 font-sans">
-                <div className="w-full space-y-4 max-w-md bg-white p-8 border text-slate-700 text-center">
-                    <h1 className="text-2xl font-semibold text-green-600">E-mail Verificado!</h1>
-                    <p className="text-slate-600">Sua conta foi ativada com sucesso.</p>
+            <main className="flex min-h-screen items-center justify-center bg-default p-2 sm:p-8 md:p-12">
+                <Card className="max-w-200" size="xl">
+                    <div className="flex w-full justify-center">
+                        {/* must revert classnames after testing */}
+                        <Image
+                            src={sao_bento_logo}
+                            alt="Logo"
+                            className="dark:hidden w-full max-w-50 h-auto mb-2"
+                        />
+                        <Image
+                            src={sao_bento_logo_dark}
+                            alt="Logo"
+                            className="hidden dark:block w-full max-w-50 h-auto mb-2"
+                        />
+                    </div>
+                    <h1 className="p-4 text-center text-2xl font-semibold text-success-fg">
+                        Email Verificado!
+                    </h1>
 
-                    <p className="text-sm text-slate-400">
-                        Entrando no dashboard em {timeLeft}s...
+                    <p className="pb-2 text-muted-fg text-center">
+                        Sua conta foi ativada com sucesso. Você já pode acessar a área do usuário.
                     </p>
 
-                    <button
-                        onClick={() => router.push('/dashboard')}
-                        className="w-full border bg-slate-50 py-2 mt-2 hover:bg-slate-100 transition-colors"
+                    <p className="pb-8 text-sm text-default-fg text-center">
+                        Redirecionando para dashboard em {timeLeft}s...
+                    </p>
+
+                    <TextButton
+                        className="w-full justify-center mb-2"
+                        onClick={() => router.replace('/dashboard')}
+                        variant="primary"
                     >
-                        Ir para o Dashboard agora
-                    </button>
-                </div>
-            </div>
+                        Voltar para dashboard agora
+                    </TextButton>
+                </Card>
+            </main>
         );
     }
 
     /* third case: the user is not logged in and we cant check if they are verified or just manually accessed this page */
     return (
-        <div className="flex min-h-screen items-center justify-center bg-slate-50 p-4 font-sans">
-            <div className="w-full space-y-4 max-w-md bg-white p-8 border text-slate-700 text-center">
-                <h1 className="text-2xl font-semibold text-green-600">E-mail Verificado!</h1>
-                <p className="text-slate-600">
-                    Sua conta foi ativada! <br />
-                    Como você está em um novo dispositivo, faça login para continuar.
+        <main className="flex min-h-screen items-center justify-center bg-default p-2 sm:p-8 md:p-12">
+            <Card className="max-w-200" size="xl">
+                <div className="flex w-full justify-center">
+                    {/* must revert classnames after testing */}
+                    <Image
+                        src={sao_bento_logo}
+                        alt="Logo"
+                        className="dark:hidden w-full max-w-50 h-auto mb-2"
+                    />
+                    <Image
+                        src={sao_bento_logo_dark}
+                        alt="Logo"
+                        className="hidden dark:block w-full max-w-50 h-auto mb-2"
+                    />
+                </div>
+                <h1 className="p-4 text-center text-2xl font-semibold text-success-fg">
+                    Email Verificado!
+                </h1>
+
+                <p className="pb-2 text-muted-fg text-center">
+                    Sua conta foi ativada com sucesso. Faça login para continuar.
                 </p>
 
-                <p className="text-sm text-slate-400">
-                    Indo para o login em {timeLeft}s...
+                <p className="pb-8 text-sm text-default-fg text-center">
+                    Redirecionando para login em {timeLeft}s...
                 </p>
 
-                <button
-                    onClick={() => router.push('/login')}
-                    className="w-full border bg-slate-50 py-2 mt-2 hover:bg-slate-100 transition-colors"
+                <TextButton
+                    className="w-full justify-center mb-2"
+                    onClick={() => router.replace('/login')}
+                    variant="primary"
                 >
-                    Ir para o Login agora
-                </button>
-            </div>
-        </div>
+                    Voltar para login agora
+                </TextButton>
+            </Card>
+        </main>
     );
 }
 
+export default function EmailVerifiedPage() {
+    return (
+        <Suspense fallback={<Loading/>}>
+            <EmailVerifiedContent />
+        </Suspense>
+    );
+}
