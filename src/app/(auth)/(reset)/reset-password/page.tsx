2e2a048bdff1e785eb10f1423d35f5f632074e43
luan-services <luandotservices@gmail.com>
2026-02-24 23:51:01 -0300

    fix: updated reset password page design, added ResetPasswordForm component to use react hook form

--- src/app/(auth)/(reset)/reset-password/page.tsx
+++ src/app/(auth)/(reset)/reset-password/page.tsx
@@ -1,22 +1,27 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { useSession } from "@/src/lib/auth-client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { resetPassword } from "@/src/lib/auth-client";
+import { toast } from "@/src/lib/toastStore";
+import { translateError } from "@/src/lib/errorMap";
 
-export default function EmailVerifiedPage() {
+import Image from "next/image";
+import sao_bento_logo from "@/public/sao-bento-logo.svg"
+import sao_bento_logo_dark from "@/public/sao-bento-logo-dark.svg"
+
+import { Card } from "@/src/components/ui/Card";
+import { TextButton } from "@/src/components/ui/TextButton";
+import { Loading } from "@/src/components/layout/Loading";
+import { ResetPasswordForm } from "@/src/components/features/ResetPasswordForm";
+import type { ResetPasswordFormData } from "@/src/components/features/ResetPasswordForm";
+
+export default function ResetPassowrdPage() {
     const router = useRouter();
     const searchParams = useSearchParams();
 
     /* get the token from the params url */
     const token = searchParams.get("token");
-
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
-
-    const [newPassword, setNewPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
     
     useEffect(() => {
         if (!token) {
@@ -26,78 +31,52 @@ export default function EmailVerifiedPage() {
 
     if (!token) {
 		return (
-			<div className="flex min-h-screen items-center justify-center bg-slate-50">
-				<p className="text-sm font-medium text-slate-500 animate-pulse">Carregando...</p>
-			</div>
+			<Loading />
 		);
     }
 
-    const handleReset = async () => {
-
-        setLoading(true);
-
-        if (!newPassword || !confirmPassword) {
-            setLoading(false);
-            return setError("Preencha todos os campos");
-        }
-
-
-        if (newPassword !== confirmPassword) {
-            setLoading(false);
-            return setError("Senhas nao coincidem");
-        }
-        const { data, error: apiError } = await resetPassword({
-            newPassword: newPassword,
+    const handleReset = async (formData: ResetPasswordFormData) => {
+        const { data, error } = await resetPassword({
+            newPassword: formData.password,
             token: token
         });
 
+        if (error?.code) {
+            toast.danger(translateError(error.code));
+            return;
+        } 
 
-        if (apiError?.message) {
-            setLoading(false);
-            return setError(apiError.message);
-        } else {
-            alert("alterado com sucesso, redirecionando para login");
-            router.push("/login");
-        }
-        
-        setLoading(false);
+        toast.success("Senha alterada com sucesso, redirecionando para login.");
+        router.push("/login");
     }
 
     return (
-        <div className="flex min-h-screen items-center justify-center bg-slate-50 p-4 font-sans">
-            <div className="w-full space-y-4 max-w-md bg-white p-8 border text-slate-700 text-center">
-                <h1 className="text-2xl font-semibold text-green-600">Reset senha</h1>
-
-                    <div>senha nova</div>
-                    <input
-                        type="password"
-                        placeholder="••••••••"
-                        value={newPassword}
-                        onChange={(e) => setNewPassword(e.target.value)}
-                        className="w-full border bg-slate-50 p-2"
-                    />
-                    <div>repetir senha nova</div>
-                    <input
-                        type="password"
-                        placeholder="••••••••"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        className="w-full border bg-slate-50 p-2"
-                    />
-
-                    <button
-                        onClick={handleReset}
-                        disabled={loading}
-                        className="w-full border bg-slate-50 py-2 mt-2 hover:bg-slate-100 transition-colors"
-                    >
-                        {loading ? "Enviando..." : "Enviar"}
-                    </button>
-
-                    {error && 
-                        <div>{error}</div>
-                    }
-            </div>
-        </div>
+        <main className="flex min-h-screen items-center justify-center bg-default p-2 sm:p-8 md:p-12">
+            <Card className="max-w-200" size="xl">
+                <div className="flex w-full justify-center">
+					{/* must revert classnames after testing */}
+					<Image
+						src={sao_bento_logo}
+						alt="Logo"
+						className="dark:hidden w-full max-w-50 h-auto mb-8"
+					/>
+					<Image
+						src={sao_bento_logo_dark}
+						alt="Logo"
+						className="hidden dark:block w-full max-w-50 h-auto mb-8"
+					/>
+				</div>
+                <h1 className="text-center text-lg text-default-fg font-semibold pb-1">
+					Alteração de senha
+				</h1>
+				<p className="text-center text-sm text-muted-fg font-medium pb-8"> 
+					O token foi efetivado com sucesso. Digite a nova senha desejada.
+				</p>
+                <div className="w-full max-w-120 justify-self-center">
+                    <ResetPasswordForm handleReset={handleReset} />
+                </div>
+            </Card>
+        </main>
     );
 }
 
---
+++ src/components/features/ResetPasswordForm.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+
+import { Input } from "../ui/Input";
+import { Button } from "../ui/Button";
+import { Label } from "../ui/Label";
+
+/* import zod and react-hook-form */
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+/* makes a zod schema almost identical to the backend one, with a few changes */
+const resetPasswordSchema = z.object({
+    password: z.string()
+        .min(8, "A senha deve ter mais de 8 caracters.")
+        .max(60, "A senha deve ter menos de 60 caracters.")
+        .regex(/[A-Z]/, "A senha deve ter pelo menos uma letra maiúscula.")
+        .regex(/[a-z]/, "A senha deve ter pelo menos uma letra minúscula.")
+        .regex(/[0-9]/, "A senha deve ter pelo menos um número.")
+        .regex(/[\W_]/, "A senha deve ter pelo menos um símbolo."),
+    confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword, { /* make sure password and confirmPassword are equals */
+    message: "As senhas não coincidem.", 
+    path: ["confirmPassword"], /* if not, confirmPassword field will throw this error */
+});
+
+/* register its type so react-hook-form useForm will be able to know which fields 'data' object has */
+export type ResetPasswordFormData = z.infer<typeof resetPasswordSchema>;
+
+interface ResetPasswordFormProps {
+    handleReset: (data: ResetPasswordFormData) => Promise<void>;
+}
+
+export const ResetPasswordForm = ({handleReset}: ResetPasswordFormProps) => {
+    const [loading, setLoading] = useState(false);
+
+    const { control, handleSubmit, formState: { errors } } = useForm<ResetPasswordFormData>({
+            resolver: zodResolver(resetPasswordSchema), /* tells useForm() which fields are allowed to exist and update error states based on the schema rules */
+            defaultValues: { /* defines the initial state of each field, controller is what actually creates the fields */
+                password: "",
+                confirmPassword: "",
+            }
+    });
+
+    const onSubmit = async (data: ResetPasswordFormData) => {
+            setLoading(true);
+            await handleReset(data);
+            setLoading(false);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+            <div className="pb-2"> 
+                <Label htmlFor="password">
+                    Senha
+                </Label>
+                <Controller
+                    name="password"
+                    control={control}
+                    render={({ field }) => (
+                        <Input 
+                            id="password"
+                            type="password"
+                            placeholder="••••••••"
+                            {...field}
+                            variant={errors.password ? "danger" : "default"}
+                        />
+                    )}
+                />
+                {errors.password && <span className="text-danger-fg text-sm">{errors.password.message}</span>}
+            </div>
+            <div className="pb-2"> 
+                <Label htmlFor="confirmPassword">
+                    Confirmar senha
+                </Label>
+                <Controller
+                    name="confirmPassword"
+                    control={control}
+                    render={({ field }) => (
+                        <Input
+                            id="confirmPassword"
+                            type="password"
+                            placeholder="••••••••"
+                            {...field}
+                            variant={errors.confirmPassword ? "danger" : "default"}
+                        />
+                    )}
+                />
+                {errors.confirmPassword && <span className="text-danger-fg text-sm">{errors.confirmPassword.message}</span>}
+            </div>
+            <Button
+                className="w-full"
+                type="submit"
+                variant="primary"
+                disabled={loading}
+            >
+                {loading ? "Enviando..." : "Salvar"}
+            </Button>
+        </form>
+    );
+};
