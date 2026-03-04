5c418b88bbce16cab4b968b2b594daf7a4b5fe38
Luan Peixoto Jardim <luandotservices@gmail.com>
2026-03-04 18:49:42 +0000

    feat: added several new icons

---
+++ src/components/icons/PulseIcon.tsx
@@ -0,0 +1,5 @@
+export const PulseIcon = ({ size = 24}: { size?: number }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M3 12h4.5L9 6l4 12 2-9 1.5 3H21"></path>
+    </svg>
+);
