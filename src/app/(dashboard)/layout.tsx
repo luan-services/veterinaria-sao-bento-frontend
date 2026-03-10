228d4aca7832fa024b7f26e8ab4c1e38fc81aafc
luan-services <luandotservices@gmail.com>
2026-03-10 18:42:35 -0300

    feat: updated dashboard auth layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,15 +1,15 @@
-import ProfileGuard from "@/src/components/features/auth/ProfileGuard"
+import { AuthGuard } from "@/src/components/features/auth/AuthGuard";
 
 /* can export metadata here if want because this is a server component */
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="min-h-screen w-full">
+        <div className="min-h-screen w-full flex">
             {/* might add navbars or sidebars here in the future */}
             <main className="min-h-screen w-full">
-                <ProfileGuard>
+                <AuthGuard>
                     {children}
-                </ProfileGuard>
+                </AuthGuard>
             </main>
         </div>
     );
